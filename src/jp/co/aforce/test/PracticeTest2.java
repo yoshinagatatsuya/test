6dcec43992aa717e8f5882499aa6e6484d507857
yoshinagatatsuya <81949672+yoshinagatatsuya@users.noreply.github.com>
2021-05-12 18:24:12 +0900

    commit

---
+++ src/jp/co/aforce/test/PracticeTest2.java
@@ -0,0 +1,24 @@
+package jp.co.aforce.test;
+
+public class PracticeTest2 {
+	public static void main(String args[]) {
+		int x=10;
+		int y=28;
+		double z= 3.14;
+		System.out.println(x*y); 
+		System.out.println(y/x); 
+		
+		double a= 87.92 ;
+		System.out.println(a);
+		System.out.println(++a);
+		int age =21;
+					
+		
+				
+	}
+
+	
+	    }
+	
+
+
